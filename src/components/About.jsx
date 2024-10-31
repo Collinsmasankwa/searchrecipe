dc9206f9262031f399199baa50df43247ecfafd2
collins <collinsmasankwa@gmail.com>
2024-10-31 19:16:06 +0300

    Changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,9 +11,7 @@ function About() {
             <div className='about-div'>
                 <h3 style={{textAlign: 'center', color: 'magenta'}}>About Us</h3>
                 <p style={{fontSize: '1.125em'}}>
-                    Welcome to <strong style={{
-                        color: 'transparent', background: 'linear-gradient(to right, #a208fb, rgb(251, 53, 4))', backgroundClip: 'text'
-                }}>SearchRecipe App</strong> – your ultimate recipe discovery hub!<br/>
+                    Welcome to <strong className='recipe-card-list-name'>SearchRecipe App</strong> – your ultimate recipe discovery hub!<br/>
                     Here, food lovers of all kinds can explore a world of flavors with just a few clicks.<br/>
                     Our platform is designed to make cooking easier and more enjoyable by helping you find<br/>
                     the perfect recipe for any occasion. Whether you&apos;re searching for a quick weekday dinner,<br/>
