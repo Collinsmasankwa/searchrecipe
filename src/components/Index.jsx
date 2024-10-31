f2317eab1e7144d78523c770ef1a5b38f64d386d
collins <collinsmasankwa@gmail.com>
2024-10-31 18:33:25 +0300

    Fix configurations for deployment

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -11,7 +11,7 @@ function Index(){
     return (
         <>
             <RecipeProvider>
-                <BrowserRouter basename='/searchrecipe'>
+                <BrowserRouter>
                     <Routes>
                         <Route path='/' element={<HeaderFooter  />}>
                             <Route index element={<Home />} />
